46207bea21e8245ac6a1b65f10c696db7604a784
Di3go7 <diegoro1994@gmail.com>
2021-08-31 20:51:22 +0200

    Commit inicial desde Android Studio

--- app/src/main/java/com/example/dam_proyecto_final/LoginActivity.java
+++ app/src/main/java/com/example/dam_proyecto_final/LoginActivity.java
@@ -2,13 +2,42 @@ package com.example.dam_proyecto_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class LoginActivity extends AppCompatActivity {
+
+    SignInButton btnsign;
+    private static final int RC_SIGN_IN = 1;
+    private GoogleSignIn mGoogleSignInClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //Instanciamos
+        btnsign = findViewById(R.id.btnSign);
+
+        // Configure Google Sign In
+//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestIdToken(getString(R.string.default_web_client_id))
+//                .requestEmail()
+//                .build();
+
+        //mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+    }
+
+    //Método de inicio de sesión de Google
+    private void signIn() {
+//        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+//        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 }
