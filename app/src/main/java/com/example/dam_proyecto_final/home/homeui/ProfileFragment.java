634b8059e306b1a31f6f487baeebeb32b63dfe72
Di3go7 <diegoro1994@gmail.com>
2021-09-12 11:26:39 +0200

    Cerrar sesión funciona correctamente. ProfileFragment editado.

--- app/src/main/java/com/example/dam_proyecto_final/home/homeui/ProfileFragment.java
+++ app/src/main/java/com/example/dam_proyecto_final/home/homeui/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.dam_proyecto_final.home.homeui;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
@@ -9,16 +10,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.dam_proyecto_final.LoginActivity;
 import com.example.dam_proyecto_final.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ProfileFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,8 +36,14 @@ public class ProfileFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    private TextView txtv;
+    private Context context;
+    private LinearLayout lytprfmydata;
+    private LinearLayout lytchangepsw;
+    private LinearLayout lytsignoff;
 
+
+//    Pruebas JJ
+//    private TextView txtv;
     private String email="vacio";
     private String pass="vacio";
 
@@ -71,7 +84,14 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        context = view.getContext();
 
+        lytprfmydata = view.findViewById(R.id.lytmydata);
+        lytchangepsw = view.findViewById(R.id.lytchangepsw);
+        lytsignoff = view.findViewById(R.id.lytsignoff);
+        lytsignoff.setOnClickListener(this);
+
+        // Pruebas JJ
         //txtv = view.findViewById(R.id.txtv);
         //txtv.setText(email +" - "+ pass);
 
@@ -83,5 +103,42 @@ public class ProfileFragment extends Fragment {
         preferencias = getActivity().getSharedPreferences("savedData", Context.MODE_PRIVATE);
         email= preferencias.getString("email","vacio");
         pass= preferencias.getString("pass","vacio");
+    }
+
+    @Override
+    public void onClick(View view) {
+        LinearLayout lyt = (LinearLayout)view;
+        switch (lyt.getId()){
+            case R.id.lytmydata:
+                //
+                break;
+            case R.id.lytchangepsw:
+                //
+                break;
+            case R.id.lytsignoff:
+                //Si tenemos login de google activo
+                GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(context);
+                if (account != null) {
+                    GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                            .requestIdToken(getString(R.string.default_web_client_id))
+                            .requestEmail()
+                            .build();
+
+                    GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(context, gso);
+                    mGoogleSignInClient.signOut();
+                }
+                //Si tenemos login por sharedpreferent
+                SharedPreferences preferencias = context.getSharedPreferences("savedData", Context.MODE_PRIVATE);
+                preferencias.edit().clear().apply();
+
+                //TODO Borrar datos sqlite
+
+
+                //Volvemos al login activity
+                Intent intent = new Intent(context, LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                break;
+        }
     }
 }
