4ea8a3f5f04bdbc1b78c8b7b9954783b86ba9f46
Javier Juarros Huerga <JJHFiles@users.noreply.github.com>
2021-09-17 20:51:33 +0200

    Insert test done javier

--- app/src/main/java/com/example/dam_proyecto_final/LoginActivity.java
+++ app/src/main/java/com/example/dam_proyecto_final/LoginActivity.java
@@ -11,19 +11,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.LongDef;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.dam_proyecto_final.home.HomeActivity;
 import com.example.dam_proyecto_final.home.model.User;
 import com.example.dam_proyecto_final.registry.RegistryActivity;
-import com.example.dam_proyecto_final.home.HomeActivity;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
-
-import java.sql.Connection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener, View.OnFocusChangeListener {
 
@@ -33,13 +43,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private static final int RC_SIGN_IN = 1, C_LOGIN_STR = 1;
     private GoogleSignInClient mGoogleSignInClient;
 
-    private Connection con;
+
 
     private User user;
 
     private EditText edtuserEmail, edtPass;
 
-    private String email = "", pass = "";
+    private String userEmail = "", userPass = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,11 +65,14 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         //Instanciamos botones establecemos el listeners
         btnsignGoogle = findViewById(R.id.btnSignGoogle);
-            btnsignGoogle.setOnClickListener(this);
+        btnsignGoogle.setOnClickListener(this);
         btnSignIn = findViewById(R.id.btnSignin);
-            btnSignIn.setOnClickListener(this);
+        btnSignIn.setOnClickListener(this);
         btnSignUp = findViewById(R.id.btnSignup);
-            btnSignUp.setOnClickListener(this);
+        btnSignUp.setOnClickListener(this);
+
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(this);
 
 
 /*
@@ -157,6 +171,11 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 startActivity(registryIntent);
                 break;
 
+            case R.id.btn:
+
+                query("jjh@gmail.es","1234","PEPE",new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+                break;
+
         }
     }
 
@@ -186,18 +205,17 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     // Chequea si ya existe el usuario creado
     private void checkSharedPreferences() {
         SharedPreferences preferencias = getSharedPreferences("savedData", Context.MODE_PRIVATE);
-        email = preferencias.getString("email", "vacio");
-        pass = preferencias.getString("pass", "vacio");
+        userEmail = preferencias.getString("email", "vacio");
+        userPass = preferencias.getString("pass", "vacio");
 
-        if (email.equals("vacio")) {
+        if (userEmail.equals("vacio")) {
             // TODO Lanzar el tutorial si el usuario no esta creado y es la primera vez que abre la app (Controlarlo también con Shared preferences).
             Toast.makeText(this, getResources().getString(R.string.sharedPreferences_empty), Toast.LENGTH_LONG).show();
 
         } else /*if(
                 edtuserEmail.getText().toString().equals(email)
                 && edtPass.getText().toString().equals(pass)
-               )*/
-         {
+               )*/ {
             signIn();
         }
     }
@@ -216,5 +234,68 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         //  createSharedPreferences();
         startActivity(signInIntent);
     }
+
+
+////////////////////////////////////////////////
+
+
+    private Button btn;
+/*
+    private final String _email = "a@a.es";
+    private final String _password = "1111";
+    private final String _user = "javi";
+    private final String _date_signup = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+*/
+    private RequestQueue requestQueue;
+
+    private static final String URL1 = "http://192.168.1.23/crud/user_insert.php";
+
+
+    public void query(final String email,final String password,final String user,final String date_signup) {
+
+        requestQueue = Volley.newRequestQueue(this);
+
+        StringRequest sr = new StringRequest(
+                Request.Method.POST,
+                URL1,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getApplicationContext(), "Response= " + response, Toast.LENGTH_LONG).show();
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), "ERROR= " + error, Toast.LENGTH_LONG).show();
+                        Log.d("DEBUGME",error.getMessage());
+                    }
+                }
+        ) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap();
+                params.put("email", email);
+                params.put("password", password);
+                params.put("name", user);
+                params.put("date_signup", date_signup);
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+       // Toast.makeText(getApplicationContext(), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())+"", Toast.LENGTH_LONG).show();
+
+        requestQueue.add(sr);
+    }
+
+
 
 }//End Class
