aafbd6e604c145655fe837d3dacd4cfc6dabca45
Di3go7 <diegoro1994@gmail.com>
2021-11-16 20:38:12 +0100

    Limpieza de codigo

--- app/src/main/java/com/example/dam_proyecto_final/ui/home/homeui/ProfileFragment.java
+++ app/src/main/java/com/example/dam_proyecto_final/ui/home/homeui/ProfileFragment.java
@@ -20,66 +20,16 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ProfileFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     private Context context;
-    private LinearLayout lytprfmydata;
-    private LinearLayout lytchangepsw;
-    private LinearLayout lytsignoff;
 
-    private TextView tvName;
-    private String email="vacio",pass="vacio",name="vacio";
+    private String userEmail ="vacio", userPass ="vacio",name="vacio";
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Comment (not used) or Rename and change types and number of parameters (if used)
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-
-        // comentar esto, no se usa
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        // fin del comentar
-
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-        readSharedPreferences();
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
